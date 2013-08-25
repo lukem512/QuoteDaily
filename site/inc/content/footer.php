63c6e7425e52bb0ae0ab0143f97e41a39c2ea84d
Luke Mitchell <Luke.Mitchell.2011@my.bristol.ac.uk>
2013-08-25 12:59:02 +0100

    Initial commit

---
+++ site/inc/content/footer.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+*
+* Website footer
+* Luke Mitchell, Nov. 2010
+*
+*/
+
+$copyrightNotice = "Released under Apache V2 License. Quotes may be used without express permission.";
+
+?>
+<div id="footer">
+	<p>
+		<?php echo $copyrightNotice; ?><br/>
+		<a href="about.php">About</a> | <a href="tos.php">Terms of service</a> | <a href="sitemap.xml">Sitemap</a> | <a href=\"https://github.com/lukem512/QuoteDaily\">Source</a>
+	</p>
+</div>
+</body>
+</html>
